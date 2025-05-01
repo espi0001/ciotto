98a91c7dabcd68251eb96d4e5863e92717c987c3
espi0001 <espi0001@stud.kea.dk>
2025-05-01 15:52:23 +0200

    added pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="relative max-h-screen h-screen">
+      <h1>about</h1>
+    </div>
+  );
+}
---
+++ src/app/ciotto-bar/page.jsx
@@ -0,0 +1,7 @@
+export default function CiottoBar() {
+  return (
+    <div className="relative max-h-screen h-screen">
+      <h1>CiottoBar</h1>
+    </div>
+  );
+}
