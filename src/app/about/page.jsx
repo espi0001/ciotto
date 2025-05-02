f7464d002f3ce6fa4b735cd10f8a9e1a4c2e845c
espi0001 <espi0001@stud.kea.dk>
2025-05-02 15:06:16 +0200

    added about page without components

---
+++ src/app/about/page.jsx
@@ -0,0 +1,98 @@
+const About = () => {
+  return (
+    <section className="grid gap-[11rem]">
+      <article className="w-full h-screen grid grid-cols-1 md:grid-cols-2">
+        <div className="flex">
+          <img className="w-full h-full object-cover" src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/663a01b6-06f2-4f2e-9aea-91115293fd9d/Screen+Shot+2024-06-21+at+3.25.53+pm.png?" alt="" />
+          <div className="w-full h-full object-cover" />
+        </div>
+        <div className="absolute flex flex-col justify-center items-center h-full w-full">
+          <h1 className="tracking-wide mb-2">
+            <span className="text-secondary-text">ABOUT</span> CIOTTO
+          </h1>
+          <p className="tracking-widest mt-2">
+            <span className="text-secondary-text">ARI PRASETYA</span> SPIROS KOUZIS
+          </p>
+        </div>
+      </article>
+
+      <article className="grid grid-cols-2 gap-[100px] px-[70px] items-center">
+        <img height={700} width={570} src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/b7bfd55b-750d-400c-ade7-4ad928bd84a7/IMG_7701+2.jpg?" alt="" />
+        <div>
+          <h2>How it started</h2>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+
+          <p className="mt-[40px]">dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim.</p>
+        </div>
+      </article>
+
+      <article className="grid grid-cols-2 gap-[100px] px-[70px] items-center">
+        <div>
+          <h2>WHO IS ARI PRASATYA</h2>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+
+          <p className="mt-[40px]">dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim.</p>
+        </div>
+        <img height={700} width={690} src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/4b34d5f9-e971-46bc-a907-266e83daee09/tempImageLz0EL9.jpg?" alt="" />
+      </article>
+
+      <article className="grid grid-cols-2 gap-[100px] px-[70px] items-center">
+        <img height={670} width={530} src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/2c40199e-b9d0-4d76-8c91-84d8b1cb1a06/tempImage1Pp5NA.png?" alt="" />
+        <div>
+          <h2>WHO IS SPIROS</h2>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed sed si</p>
+          <br />
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed sed si</p>
+        </div>
+      </article>
+
+      <article className="px-[70px] items-center">
+        <div className="flex gap-12">
+          <h2 className="content-end border-b-1 border-r-1">
+            WHO ARE WE <br />
+            WORKING WITH
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            <div className="flex flex-col items-center">
+              <img className="mb-2 w-full h-[350px] object-cover" src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/2bb6df1a-0a1e-4460-a043-90754523f544/IMG_7700.jpg?format=2500w" alt="GATTI - SALAKA" />
+              <h3 className="font-semibold text-sm mb-1">GATTI - SALAMA</h3>
+              <p className="text-xs text-center">Description of partnership or collaborator.</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="mb-2 w-full h-[350px] object-cover" src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/b7bfd55b-750d-400c-ade7-4ad928bd84a7/IMG_7701+2.jpg?format=2500w" alt="JOMSEE - TEA" />
+              <h3 className="font-semibold text-sm mb-1">JOMSEE - TEA</h3>
+              <p className="text-xs text-center">Description of partnership or collaborator.</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="mb-2 w-full h-[350px] object-cover" src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/b020ba90-025d-4156-b148-f2835cd9d4ea/Andra+in.jpg?format=2500w" alt="HOUSE OF NORDIC" />
+              <h3 className="font-semibold text-sm mb-1">ANDRA EATERY</h3>
+              <p className="text-xs text-center">Description of partnership or collaborator.</p>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className="grid grid-cols-1 md:grid-cols-2 gap-[100px] items-center px-4 mt-[8rem]">
+        <img className="w-full" src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/65053e0b-e935-437f-8868-aa0b2af8e4a9/IMG_2542.jpeg?format=1500w" alt="Chair" />
+        <div>
+          <h2 className="mb-4 text-center">SUSTAINABILITY</h2>
+          <p className="mb-2">dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <br />
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+          <p>dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor. dignissim, cursus urna non, id Donec enim. Nam viverra sit dui. nibh eu enim. nec id dui vehicula, sed adipiscing tortor.</p>
+        </div>
+      </article>
+      <article className="px-4 mt-8">
+        <img className="w-full h-96 object-cover " src="https://images.squarespace-cdn.com/content/v1/66747988c4481f654ec86537/c4486bc0-4f96-402c-92a7-31711cdeca14/LYFA_SS22_12.JPG?format=2500w" alt="Espresso and Shelf" />
+      </article>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/app/works/[id]/page.jsx
@@ -0,0 +1,9 @@
+// import { createClient } from "../../../utils/supabase/server";
+// import ProductHeader from "../../../components/ProductDetails/ProductHeader";
+// import ProductDescription from "../../../components/ProductDetails/ProductDescription";
+// import ProductImages from "../../../components/ProductDetails/ProductImages";
+// import ProductQuantity from "../../../components/ProductDetails/ProductQuantity";
+// import ProductSpecs from "../../../components/ProductDetails/ProductSpecs";
+// import RelatedProducts from "../../../components/ProductDetails/RelatedProducts";
+
+export default async function WorkDetail({ params }) {}
