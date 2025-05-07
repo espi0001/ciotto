c082ea306ed0d5dc2119d508dc6824ae6ce4f3c9
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-07 12:28:04 +0200

    Test Vercel deployment

--- src/components/02-molecules/InputField.jsx
+++ src/components/02-molecules/InputField.jsx
@@ -9,7 +9,7 @@ export default function InputField({ label, id, name, type = "text", textarea = 
           id={id} //
           name={name}
           rows={rows}
-          className="bg-transparent border border-beige-100/50 py-1 px-2 focus:outline-none focus:border-secondary-text transition-colors resize-none"
+          className="bg-transparent border border-beige-100/50 rounded-xl py-1 px-2 focus:outline-none focus:border-secondary-text transition-colors resize-none"
         />
       ) : (
         <input
