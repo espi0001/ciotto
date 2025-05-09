e224d5bb765f83fbe2be2c65b4a85af8a757380c
espi0001 <espi0001@stud.kea.dk>
2025-05-09 12:16:50 +0200

    ændret grid og gap på produkter

--- src/components/04-templates/ProductGrid.jsx
+++ src/components/04-templates/ProductGrid.jsx
@@ -2,7 +2,7 @@ import ProductCard from "../02-molecules/ProductCard";
 import PropTypes from "prop-types";
 
 const ProductGrid = ({ products, small = false }) => (
-  <div className={small ? "grid grid-cols-2 md:grid-cols-4 gap-[48px]" : "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[48px]"}>
+  <div className={small ? "grid grid-cols-2 md:grid-cols-4 gap-x-[48px] gap-y-[18px]" : "grid grid-cols-2 lg:grid-cols-3 gap-y-[48px] gap-x-[18px]"}>
     {products.map((product) => (
       <ProductCard key={product.id} image={product.image} title={product.title} link={product.link} small={small} />
     ))}
