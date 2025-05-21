281a39a198b269dd33be25193cf0debaf8eab100
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-22 00:36:48 +0200

    Update Logo component to change font weight from bold to medium for improved visual consistency.

--- src/components/Navigation/Logo.jsx
+++ src/components/Navigation/Logo.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Logo({ className = "", size = "text-4xl", color = "primary-text", ...props }) {
   return (
-    <Link href="/" className={`font-bold tracking-wide ${size} ${className}`} style={{ color }} {...props}>
+    <Link href="/" className={`font-medium tracking-wide ${size} ${className}`} style={{ color }} {...props}>
       CIOTTO
     </Link>
   );
