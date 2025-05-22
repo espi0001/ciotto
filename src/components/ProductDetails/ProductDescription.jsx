e53e16b9c53836d5d8c4a2ae55ec988d040779e9
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-22 14:23:54 +0200

    Refactor ProductDescription component to improve code readability by removing unnecessary class attributes. Maintain existing functionality for displaying product description, price, and contact button.

--- src/components/ProductDetails/ProductDescription.jsx
+++ src/components/ProductDetails/ProductDescription.jsx
@@ -6,18 +6,18 @@ const ProductDescription = ({ description, price }) => {
     // w-full lg:w-[207px]
     <div className="mt-[70px]">
       <Copy delay={1}>
-        <div className="">
+        <div>
           <h2 className="mb-4 font-bold h3-product">Description:</h2>
           <p className="">{description}</p>
         </div>
-        <div className="flex flex-col gap-6">
-          <p className="font-semibold mt-6">Only available to purchase by contacting Ciotto.</p>
-          <p>{price}</p>
-          <Button className="text-xl body-text" size="large" variant="primary" link="/contact">
-            Contact to purchase
-          </Button>
-        </div>
       </Copy>
+      <div className="flex flex-col gap-6">
+        <p className="font-semibold mt-6">Only available to purchase by contacting Ciotto.</p>
+        <p>{price}</p>
+        <Button className="text-xl body-text" size="large" variant="primary" link="/contact">
+          Contact to purchase
+        </Button>
+      </div>
     </div>
   );
 };
