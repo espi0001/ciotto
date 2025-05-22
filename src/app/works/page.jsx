666cb117948233c7efdecd0e7fee41b6d620eeb8
espi0001 <espi0001@stud.kea.dk>
2025-05-22 13:35:54 +0200

    Refactor Works page layout and WorkCard component for improved responsiveness and visual consistency. Adjusted grid layout in Works page and enhanced WorkCard aspect ratios based on size.

--- src/app/works/page.jsx
+++ src/app/works/page.jsx
@@ -1,10 +1,9 @@
 export const dynamic = "force-dynamic";
-
-import WorkCard from "@/components/WorksDetails/WorkCard";
-import TitleSection from "@/components/Sections/TitleSection";
 import { getAll } from "@/utils/supabase/api";
+import TitleSection from "@/components/Sections/TitleSection";
+import WorkCard from "@/components/WorksDetails/WorkCard";
 
-const isLargeCard = (index) => [1, 2, 5, 6].includes(index);
+const isLargeCard = (index) => [1, 2, 5, 6].includes(index); // Husk 0-indeks!
 
 export const metadata = {
   title: "Ciotto | Works",
@@ -15,12 +14,19 @@ export default async function Works() {
   const works = await getAll("works");
 
   return (
-    <section className="px-section">
+    <section className="px-section max-w-screen-2xl mx-auto ">
       <TitleSection title="WORKS OF ARI PRASETYA" />
 
-      <article className="section-spacing flex flex-wrap justify-between gap-y-[2.5rem] md:gap-y-[11rem] gap-10 mb-48">
+      <article className="section-spacing grid grid-cols-2 gap-y-10 gap-x-6 md:gap-x-20 lg:gap-x-30 mb-48">
         {works.map((work, index) => (
-          <WorkCard key={work.id} image={work.heroimage} title={work.name} number={index + 1} link={`/works/${work.slug}`} size={isLargeCard(index) ? "big" : "small"} />
+          <WorkCard
+            key={work.id} //
+            image={work.heroimage}
+            title={work.name}
+            number={index + 1}
+            link={`/works/${work.slug}`}
+            size={isLargeCard(index) ? "big" : "small"}
+          />
         ))}
       </article>
     </section>
