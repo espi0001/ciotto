2a60ae8571be1363e6c6475e8b3fd841ba4f67df
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-22 15:02:43 +0200

    Update section spacing in TitleSection and adjust global styles for improved layout consistency.

--- src/components/Sections/TitleSection.jsx
+++ src/components/Sections/TitleSection.jsx
@@ -2,7 +2,7 @@ import Copy from "../Animations/gsap-anim/TextAnimation";
 
 function TitleSection({ title, description }) {
   return (
-    <div className="mt-28">
+    <div className="section-spacing-small">
       <Copy delay={1}>
         <h1 className="h1-large font-semibold mb-[10px]">{title}</h1>
       </Copy>
