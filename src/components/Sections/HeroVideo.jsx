3359102b37f58c1b4eba880e39cff5c967701a6c
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-23 18:26:58 +0200

    Enhance HeroVideo component by adding playsInline and tabIndex attributes to the video element for improved accessibility and functionality.

--- src/components/Sections/HeroVideo.jsx
+++ src/components/Sections/HeroVideo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeroVideo({ src, className = "", ...props }) {
   return (
     <article className={`relative h-full md:h-screen max-h-[700px] ${className}`} {...props}>
-      <video autoPlay muted loop className="w-full h-full object-cover">
+      <video autoPlay muted loop playsInline tabIndex={-1} style={{ pointerEvents: "none" }} className="w-full h-full object-cover">
         <source src={src} type="video/mp4" />
       </video>
     </article>
