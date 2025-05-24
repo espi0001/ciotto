6d17250fd01e784165bc6bb6eeba7bdea9990016
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-24 20:41:17 +0200

    Enhance HeroVideo component with client-side rendering; implement fade-in effect for text using state and useEffect for improved user experience.

--- src/components/Sections/HeroVideo.jsx
+++ src/components/Sections/HeroVideo.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 export default function HeroVideo({ src, text1, text2, className = "", ...props }) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   return (
     <article className={`relative h-full md:h-screen max-h-[700px] ${className}`} {...props}>
       <video autoPlay muted loop playsInline tabIndex={-1} style={{ pointerEvents: "none" }} className="w-full h-full object-cover">
         <source src={src} type="video/mp4" />
       </video>
-      <h1 className="body-text text-secondary-text absolute bottom-0 right-0 flex flex-col">
-        {text1} <span>{text2}</span>
+      <h1 className={`px-section h2-work-details opacity-0 transition-opacity duration-3000 ease-in-out text-secondary-text absolute bottom-30 flex flex-col ${show ? "opacity-100" : ""}`}>
+        {text1} <span className="h2-work-details">{text2}</span>
       </h1>
     </article>
   );
