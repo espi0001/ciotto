53ff878d661025a4aa7d413bc386465e893465b2
Sebastian Elias <seen0001@stud.kea.dk>
2025-05-26 12:47:15 +0200

    Refactor ProductDescription component to enhance structure and readability by wrapping text elements in Copy components for improved animation and presentation.

--- src/components/ProductDetails/ProductDescription.jsx
+++ src/components/ProductDetails/ProductDescription.jsx
@@ -5,15 +5,19 @@ const ProductDescription = ({ description, price }) => {
   return (
     // w-full lg:w-[207px]
     <div className="md:mt-[70px]">
-      <Copy startAtTenPercent="true" delay={1}>
-        <div>
+      <div>
+        <Copy startAtTenPercent="true" delay={1}>
           <h2 className="mb-4 font-bold h3-product">Description:</h2>
           <p className="max-w-[64ch]">{description}</p>
-        </div>
-      </Copy>
+        </Copy>
+      </div>
       <div className="flex flex-col gap-6 mb-6">
-        <p className="font-semibold mt-6">Only available to purchase by contacting Ciotto.</p>
-        <p>{price}</p>
+        <Copy startAtTenPercent="true" delay={1}>
+          <p className="font-semibold mt-6">Only available to purchase by contacting Ciotto.</p>
+        </Copy>
+        <Copy startAtTenPercent="true" delay={1}>
+          <p>{price}</p>
+        </Copy>
         <Button className="text-xl body-text" size="large" variant="primary" link="/contact">
           Contact to purchase
         </Button>
